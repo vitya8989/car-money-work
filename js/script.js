a14de2586c76c739894e5d0d412c4987748f0d5d
vitya8989 <vitya898989@gmail.com>
2021-09-21 17:42:01 +0300

    two card

--- js/script.js
+++ js/script.js
@@ -47,3 +47,72 @@ new Swiper('.slider-how', {
 		},
 	},
 });
+
+let range1 = document.querySelector('.range-1');
+let round1 = document.querySelector('.round-1');
+let shadow1 = document.querySelector('.shadow-1');
+let leftRound1 = range1.value / 0.75 - 16;
+let widthShadow1 = leftRound1 + 1;
+let rangeOutputs1 = document.querySelectorAll('.range-output1');
+round1.style.left = `${leftRound1}%`;
+shadow1.style.width = `${widthShadow1}%`;
+range1.oninput = function () {
+	leftRound1 = range1.value / 0.75 - 16;
+	if (window.innerWidth < 501) {
+		leftRound1 = range1.value / 0.76 - 16;
+	}
+	widthShadow1 = leftRound1 + 1;
+	round1.style.left = `${leftRound1}%`;
+	shadow1.style.width = `${widthShadow1}%`;
+	for (let i = 0; i < rangeOutputs1.length; i++) {
+		if (range1.value > 60 || range1.value == 24) {
+			rangeOutputs1[i].innerHTML = `${range1.value} месяца`;
+		} else {
+			rangeOutputs1[i].innerHTML = `${range1.value} месяцев`;
+		}
+	}
+}
+
+let range2 = document.querySelector('.range-2');
+let round2 = document.querySelector('.round-2');
+let shadow2 = document.querySelector('.shadow-2');
+let leftRound2 = range2.value / 10.4;
+let widthShadow2 = leftRound2 + 1;
+let rangeOutput2 = document.querySelector('.range-output2');
+round2.style.left = `${leftRound2}%`;
+shadow2.style.width = `${widthShadow2}%`;
+range2.oninput = function () {
+	leftRound2 = range2.value / 10.4;
+	widthShadow2 = leftRound2 + 1;
+	round2.style.left = `${leftRound2}%`;
+	shadow2.style.width = `${widthShadow2}%`;
+	if (range2.value == 0) {
+		rangeOutput2.innerHTML = `${range2.value} &#8381`;
+	} else {
+		rangeOutput2.innerHTML = `${range2.value} 000&#8381`;
+	}
+}
+
+let toFirstBtn = document.querySelector('.to-first-btn');
+let toSecondBtn = document.querySelector('.to-second-btn');
+let toThirdBtn = document.querySelector('.to-third-btn');
+let itemCredit1 = document.querySelector('.item-credit1');
+let itemCredit2 = document.querySelector('.item-credit2');
+let itemCredit3 = document.querySelector('.item-credit3');
+toFirstBtn.onclick = function () {
+	event.preventDefault();
+	itemCredit2.classList.remove('active-credit');
+	itemCredit1.classList.add('active-credit');
+}
+toSecondBtn.onclick = function () {
+	event.preventDefault();
+	itemCredit1.classList.remove('active-credit');
+	itemCredit2.classList.add('active-credit');
+}
+toThirdBtn.onclick = function () {
+	event.preventDefault();
+	itemCredit2.classList.remove('active-credit');
+	itemCredit3.classList.add('active-credit');
+}
+
+
