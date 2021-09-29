d58be6e77dded57f5dfb7d1844d88d142ce0d156
vitya8989 <vitya898989@gmail.com>
2021-09-29 16:27:53 +0300

    partners full

---
+++ js/partners.js
@@ -0,0 +1,89 @@
+function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	} else {
+		document.querySelector('body').classList.add('no-webp');
+	}
+});
+
+let navBurger = document.querySelector('.nav__burger');
+let navContent = document.querySelector('.nav__content');
+let navLink = document.querySelectorAll('.nav__link');
+navBurger.onclick = function () {
+	navBurger.classList.toggle('burger-open');
+	navContent.classList.toggle('nav_mobile-open');
+}
+for (let i = 0; i < navLink.length; i++) {
+	navLink[i].onclick = function () {
+		navBurger.classList.remove('burger-open');
+		navContent.classList.remove('nav_mobile-open');
+	}
+}
+
+document.querySelectorAll('a[href^="#"').forEach(link => {
+
+	link.addEventListener('click', function (e) {
+		e.preventDefault();
+
+		let href = this.getAttribute('href').substring(1);
+
+		const scrollTarget = document.getElementById(href);
+
+		let topOffset = document.querySelector('.nav__container').offsetHeight;
+
+		const elementPosition = scrollTarget.getBoundingClientRect().top;
+		const offsetPosition = elementPosition - topOffset;
+		window.scrollBy({
+			top: offsetPosition,
+			behavior: 'smooth'
+		});
+	});
+});
+
+new Swiper('.partners-slider', {
+	slidesPerView: 'auto',
+	breakpoints: {
+		769: {
+			slidesPerView: 2,
+		},
+	},
+});
+
+new Swiper('.instagram-slider', {
+	slidesPerView: 'auto',
+});
+
+// let scriptMap = document.createElement('script');
+// scriptMap.src = 'https://api-maps.yandex.ru/2.1/?apikey=&lang=ru_RU';
+// setTimeout(() => document.head.append(scriptMap), 2000);
+// scriptMap.onload = function () {
+// 	ymaps.ready(init);
+// };
+
+// function init() {
+// 	var myMap = new ymaps.Map("map", {
+// 		center: [55.752648, 37.603846],
+// 		zoom: 17
+// 	});
+// 	var placemark = new ymaps.Placemark([55.752648, 37.603846], {
+// 		iconColor: 'blue'
+// 	}
+// 	);
+// 	myMap.geoObjects.add(placemark);
+// }
+
+let footerMap = document.querySelector('.footer__map');
+let footerBottom = document.querySelector('.footer__bottom');
+if (window.innerWidth < 726) {
+	footerBottom.appendChild(footerMap);
+}
